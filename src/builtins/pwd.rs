1550b8bd64b17b89ff138fb2ae5c3c90dbd9c49c
Avery Li <2594167452@qq.com>
2024-10-27 16:56:47 +0800

    Finish adding Chinese comments for pwd.rs

--- src/builtins/pwd.rs
+++ src/builtins/pwd.rs
@@ -5,6 +5,7 @@ use super::prelude::*;
 use crate::{env::Environment, wutil::wrealpath};
 
 // The pwd builtin. Respect -P to resolve symbolic links. Respect -L to not do that (the default).
+// 设置pwd命令选项，解析符号连接（Unix中又名软连接，类似于windows中的快速连接）还是不解析
 const short_options: &wstr = L!("LPh");
 const long_options: &[WOption] = &[
     wopt(L!("help"), NoArgument, 'h'),
@@ -17,7 +18,7 @@ pub fn pwd(parser: &Parser, streams: &mut IoStreams, argv: &mut [&wstr]) -> Opti
     let argc = argv.len();
     let mut resolve_symlinks = false;
     let mut w = WGetopter::new(short_options, long_options, argv);
-    while let Some(opt) = w.next_opt() {
+    while let Some(opt) = w.next_opt() {  // 循环获取命令行中的选项参数
         match opt {
             'L' => resolve_symlinks = false,
             'P' => resolve_symlinks = true,
@@ -33,6 +34,7 @@ pub fn pwd(parser: &Parser, streams: &mut IoStreams, argv: &mut [&wstr]) -> Opti
         }
     }
 
+    // 命令行中有多余的选项参数，进行错误处理
     if w.wopt_index != argc {
         streams
             .err
@@ -41,10 +43,10 @@ pub fn pwd(parser: &Parser, streams: &mut IoStreams, argv: &mut [&wstr]) -> Opti
     }
 
     let mut pwd = WString::new();
-    if let Some(tmp) = parser.vars().get(L!("PWD")) {
+    if let Some(tmp) = parser.vars().get(L!("PWD")) {  // 从环境中获取PWD的值，赋给pwd，但没有进行符链接解析
         pwd = tmp.as_string();
     }
-    if resolve_symlinks {
+    if resolve_symlinks {  // 进行符号链接解析，获取物理路径
         if let Some(real_pwd) = wrealpath(&pwd) {
             pwd = real_pwd;
         } else {
